f120bde2f45c55f102ee9951f9d1656e6c0b88c0
Caio Bernardo <cbpereira50@gmail.com>
2021-07-24 20:30:47 -0300

    Paint App

---
+++ app/src/main/java/com/example/mypaint/ui/activity/Principal_activity.java
@@ -0,0 +1,16 @@
+package com.example.mypaint.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mypaint.R;
+
+public class Principal_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
